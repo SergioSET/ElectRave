11926dcd45fda054a2683f5e9bf54fa51da5aaa0
juanes273 <75282431+juanes273@users.noreply.github.com>
2023-01-15 23:27:13 +0000

    Cambio de estructura

---
+++ authentication/views.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import login, authenticate, logout # add to imports
+from django.shortcuts import render, redirect
+from . import forms
+from django.conf import settings
+
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+        message = 'Usuario o contraseña no validos'
+    return render(request, 'authentication\login.html', context={'form': form, 'message': message})
+
+# C:\Users\juane\Desktop\Nueva carpeta\App\authentication\templates\authentication\login.html
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # auto-login user
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form': form})
